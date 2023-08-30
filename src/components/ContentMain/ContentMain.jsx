b72751d8f1be28e62bebf6346e7b2a2719994dee
z297xu <kyrie807782352@gmail.com>
2023-08-30 12:48:34 -0400

    add contentMain component css style

---
+++ src/components/ContentMain/ContentMain.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ContentMain.css'
+import Cards from '../Cards/Cards'
+
+export default function ContentMain() {
+  return (
+    <div className='main-content-holder'>
+        <div className="content-grid-one">
+            <Cards />
+        </div>
+    </div>
+  )
+}
