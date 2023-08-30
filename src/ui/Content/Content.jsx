22e1dbc19b09caf72e039192076fcd2f146c31c1
z297xu <kyrie807782352@gmail.com>
2023-08-30 12:48:45 -0400

    add Content component

--- src/ui/Content/Content.jsx
+++ src/ui/Content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Content.css'
 import ContentTop from '../../components/ContentTop/ContentTop'
+import ContentMain from '../../components/ContentMain/ContentMain'
 
 export default function Content() {
   return (
     <div className='main-content'>
       <ContentTop />
+      <ContentMain />
     </div>
   )
 }
