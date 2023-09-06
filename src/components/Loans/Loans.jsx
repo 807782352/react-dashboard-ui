9f99459609912f932721ca5e43b9a833f6f8601e
z297xu <kyrie807782352@gmail.com>
2023-09-05 23:02:13 -0400

    fix some bugs in loans components

--- src/components/Loans/Loans.jsx
+++ src/components/Loans/Loans.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { iconsImgs } from '../../utils/images'
 import './Loans.css'
-
+import { loans } from '../../data/data'
+ 
 export default function Loans() {
+
+  const percent = (loans.savings * 100 / loans.target).toFixed(1);
+
   return (
     <div className='subgrid-two-item grid-common grid-c7'>
       <div className="grid-c-title">
@@ -17,22 +21,22 @@ export default function Loans() {
             <svg>
               <circle cx='105' cy='105' r='50'></circle>
               <circle cx='105' cy='105' r='50' style={{
-                "--percent": "50"
+                "--percent": `${percent}`
               }}></circle>
             </svg>
             <div className="number">
-              <h3>50 <span>%</span></h3>
+              <h3>{percent} <span>%</span></h3>
             </div>
           </div>
         </div>
         <ul className="data-list">
           <li className="data-item text-silver-v1">
             <span className="data-item-text">Savings</span>
-            <span className="data-item-value">$500, 000</span>
+            <span className="data-item-value">$ {loans.savings}</span>
           </li>
           <li className="data-item text-silver-v1">
             <span className="data-item-text">Target Reached</span>
-            <span className="data-item-value">$250, 000</span>
+            <span className="data-item-value">$ {loans.target}</span>
           </li>
         </ul>
       </div>
